63ac2b4c8b42af7944ff486c64e0cbd42e8bb6d5
Rakhmatjon Khasanov <rakhmatjon.khasanov2711@gmail.com>
2020-07-18 23:08:34 +0500

    Add a general availability test

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -5,34 +5,46 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
 from models import setup_db, Question, Category
+from sqlalchemy import desc
 
 
 class TriviaTestCase(unittest.TestCase):
-    """This class represents the trivia test case"""
-
+    
     def setUp(self):
-        """Define test variables and initialize app."""
+    
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "{}://{}:{}@{}/{}".format('postgresql','postgres','0820','localhost:5432', 'trivia_test')
         setup_db(self.app, self.database_path)
 
-        # binds the app to the current context
         with self.app.app_context():
             self.db = SQLAlchemy()
             self.db.init_app(self.app)
-            # create all tables
+            
             self.db.create_all()
     
     def tearDown(self):
-        """Executed after reach test"""
+        
         pass
 
     """
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+
+#----------------------------------------------------------------------------#
+# General Test
+#----------------------------------------------------------------------------#
+    
+    def test_endpoint_not_available(self):
+        """Test getting an endpoint which does not exist """
+        res = self.client().get('/DoesNotExist')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['error'], 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Resource Not Found')
 
 
 # Make the tests conveniently executable
