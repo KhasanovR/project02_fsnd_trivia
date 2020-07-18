33b2101f5c88cbaf7b4f56ea535258bdf906dc03
Rakhmatjon Khasanov <rakhmatjon.khasanov2711@gmail.com>
2020-07-18 23:16:17 +0500

    Add a 400-test for GET endpoint on /categories/<string:category_id>/questions

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -60,6 +60,14 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['total_questions'] > 0)
         self.assertEqual(data['current_category'], '1')
 
+    def test_400_get_questions_from_category(self):
+        res = self.client().get('/categories/14125412/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 400)
+        self.assertEqual(data['message'], 'No questions with category 14125412 found.')
 
 if __name__ == "__main__":
     unittest.main()
