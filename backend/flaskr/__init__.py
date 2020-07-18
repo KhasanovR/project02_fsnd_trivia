b9214a3403ae6954c41e713d6bf90cee70682823
Rakhmatjon Khasanov <rakhmatjon.khasanov2711@gmail.com>
2020-07-18 19:34:15 +0500

    Create an endpoint to DELETE question using a question ID

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -63,12 +63,24 @@ def create_app(test_config=None):
   '''
 
   '''
-  @TODO: 
-  Create an endpoint to DELETE question using a question ID. 
+  @TODO(Done): Create an endpoint to DELETE question using a question ID. 
 
   TEST: When you click the trash icon next to a question, the question will be removed.
   This removal will persist in the database and when you refresh the page. 
   '''
+  @app.route('/questions/<int:question_id>', methods=['DELETE'])
+  def delete_questions(question_id):
+    question = Question.query.get(question_id)
+    if not question:
+      abort(400, {'message': 'Question ID {}: Not Found'.format(question_id)})
+    try:
+      question.delete()
+      return jsonify({
+        'success': True,
+        'deleted': question_id
+      })
+    except:
+      abort(422)
 
   '''
   @TODO: 
